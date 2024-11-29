f49b542ae850c689f464835d11e6010f8463087b
daynios <64890512+Daynios@users.noreply.github.com>
2024-11-29 10:37:57 +0000

    add player sprite with animations
    also adds flashing animation for the powerup tiles.

--- main.py
+++ main.py
@@ -18,6 +18,28 @@ PI = math.pi # sets pi as a variable, so that it can be easily called in future 
 
 colour = 'blue' #colour for all of the tilemap shapes. (excl powerups & coins.)
 
+player_images = []
+for i in range(1,5):
+    player_images.append(pygame.transform.scale(pygame.image.load(f'assets/player_images/{i}.png'), (45,45)))
+player_x = 450 # Player starting position x
+player_y = 663# Player starting position y
+direction = 0 # Player starting direction (right)
+counter = 0
+flicker = False # flicker for powerup tiles
+
+
+
+def drawPlayer():
+    if direction == 0:
+        screen.blit(player_images[counter // 5], (player_x, player_y)) # RIGHT
+    elif direction == 1:
+        screen.blit(pygame.transform.flip(player_images[counter // 5], True, False), (player_x, player_y)) # LEFT
+    elif direction == 2:
+        screen.blit(pygame.transform.rotate(player_images[counter // 5], 90), (player_x, player_y)) # UP
+    elif direction == 3:
+        screen.blit(pygame.transform.rotate(player_images[counter // 5], 270), (player_x, player_y)) # DOWN
+
+
 
 #drawing the tilemap board
 
@@ -31,7 +53,7 @@ def drawBoard():
         for j in range(len(level[i])):
             if level[i][j] == 1:
                 pygame.draw.circle(screen, 'white', (j * num2 + (0.5*num2), i * num1 + (0.5*num1)), 4) #draw small circles (coins)
-            if level[i][j] == 2:
+            if level[i][j] == 2 and not flicker:
                 pygame.draw.circle(screen, 'gold', (j * num2 + (0.5*num2), i * num1 + (0.5*num1)), 10) #draw large circles (powerups)
             if level[i][j] == 3:
                 pygame.draw.line(screen, colour, (j * num2 + (0.5*num2), i * num1), (j * num2 + (0.5*num2), i * num1 + num1), 3) # draw vertical walls
@@ -53,12 +75,32 @@ def drawBoard():
 run = True
 while run:
     timer.tick(fps)
+    if counter < 19:
+        counter += 1
+        if counter > 3:
+            flicker = False
+        
+    else:
+        counter = 0
+        flicker = True
+
     screen.fill('black')
     drawBoard()
+    drawPlayer()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+        if event.type == pygame.KEYDOWN:    #Register direction changes with arrow keys
+            if event.key == pygame.K_RIGHT:
+                direction = 0
+            if event.key == pygame.K_LEFT:
+                direction = 1
+            if event.key == pygame.K_UP:
+                direction = 2
+            if event.key == pygame.K_DOWN:
+                direction = 3
 
     pygame.display.flip()
 pygame.quit()
